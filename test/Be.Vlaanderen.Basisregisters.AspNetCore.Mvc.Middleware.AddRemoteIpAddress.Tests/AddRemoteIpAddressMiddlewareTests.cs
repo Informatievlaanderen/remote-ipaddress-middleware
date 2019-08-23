573d3be40322383f5b8e3723536c6493b8d1cda4
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-08-23 13:04:55 +0200

    test: add tests fixes #1

---
+++ test/Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Middleware.AddRemoteIpAddress.Tests/AddRemoteIpAddressMiddlewareTests.cs
@@ -0,0 +1,23 @@
+namespace Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Middleware.AddRemoteIpAddress.Tests
+{
+    using System.Net;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Xunit;
+
+    public class AddRemoteIpAddressMiddlewareTests
+    {
+        [Fact]
+        public async Task AddsExpectedIPToUserClaims()
+        {
+            var middleware = new AddRemoteIpAddressMiddleware(innerContext => Task.CompletedTask);
+            var context = new DefaultHttpContext();
+            var ipString = "123.245.123.245";
+            context.Connection.RemoteIpAddress = IPAddress.Parse(ipString);
+
+            await middleware.Invoke(context);
+
+            context.User.HasClaim(AddRemoteIpAddressMiddleware.UrnBasisregistersVlaanderenIp, ipString);
+        }
+    }
+}
